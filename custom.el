e394e6d26309383deb200815762606abd7485585
Benjamin Frazer <benjamin.frazer.1997@gmail.com>
2022-11-19 12:18:29 +0000

    added additional save local variables

--- custom.el
+++ custom.el
@@ -4,7 +4,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ignored-local-variable-values '((line-spacing . 0.1)))
- '(safe-local-variable-values '((line-spacing . 0.2) (line-spacing . 0.1))))
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook
+           (lambda nil
+             (if
+                 (y-or-n-p "Tangle?")
+                 (org-babel-tangle)))
+           nil t)
+     (eval add-hook 'after-save-hook
+           (lambda nil
+             (if
+                 (y-or-n-p "Reload?")
+                 (load-file user-init-file)))
+           nil t)
+     (project-vc-merge-submodules)
+     (line-spacing . 0.2)
+     (line-spacing . 0.1))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,4 +29,5 @@
  '(org-level-2 ((t (:inherit outline-2 :height 1.2 :weight semi-bold))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.1 :weight semi-bold))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.0 :weight semi-bold))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+ '(org-link ((t (:inherit link :foreground "maroon")))))
